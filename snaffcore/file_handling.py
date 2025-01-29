849253db0d05bb4f9706919fdc8720971c32e2fb
Michaeladsl <89179287+Michaeladsl@users.noreply.github.com>
2025-01-29 14:05:27 -0500

    Update file_handling.py

--- snaffcore/file_handling.py
+++ snaffcore/file_handling.py
@@ -64,47 +64,41 @@ class RemoteFile():
         return f'\\\\{self.target}\\{self.share}\\{self.name}'
 
     def handle_download_error(self, dir_path, err, is_from_go_loud: bool, add_err: bool):
-        # subfiles = []
-        if str(err).find("DIRECTORY"):
+        if "DIRECTORY" in str(err):  # Checking if the error is due to a directory
             dir_text = termcolor.colored("[Directory]", 'light_blue')
-
+    
             if is_from_go_loud:
-                log.info(
-                    f"{dir_text} \\\\{self.target}\\{self.share}\\{dir_path}")
+                log.info(f"{dir_text} \\\\{self.target}\\{self.share}\\{dir_path}")
+    
             try:
                 subfiles = self.smb_client.ls(self.share, str(dir_path))
-                add_err = False
-            
-
+                add_err = False  # Reset error flag
+    
                 for subfile in subfiles:
                     sub_size = subfile.get_filesize()
                     sub_name = str(dir_path + "\\" + subfile.get_longname())
-
+    
                     try:
-                        subfile = RemoteFile(
-                            sub_name, self.share, self.target, sub_size)
+                        subfile = RemoteFile(sub_name, self.share, self.target, sub_size)
                         if is_from_go_loud:
                             subfile.get(self.smb_client)
-                        # else: 
-                            # is_interest_file(self, self.smb_client, self.share)
                         add_err = False
-
+    
                     except FileRetrievalError as e:
-                        # handle_impacket_error(e, subfile.smb_client, subfile.share, sub_name, True)
                         err = e
                         add_err = True
-
+    
                     finally:
                         if add_err:
-                            # print(error)
-                            self.handle_download_error(
-                                sub_name, err, is_from_go_loud, True)
+                            try:
+                                self.handle_download_error(sub_name, err, is_from_go_loud, True)
+                            except Exception as err:
+                                log.error(f"Nested error handling failed: {err}")
                         else:
                             file_text = termcolor.colored("[File]", 'green')
                             if is_from_go_loud:
-                                log.info(
-                                    f"{file_text} \\\\{self.target}\\{self.share}\\{sub_name}")
-            except FileListError as e:
+                                log.info(f"{file_text} \\\\{self.target}\\{self.share}\\{sub_name}")
+    
+            except FileListError:
                 if is_from_go_loud:
-                    log.error(
-                        f"Access denied, cannot read at {self.target}\\{self.share}\\{dir_path}")
+                    log.error(f"Access denied, cannot read at {self.target}\\{self.share}\\{dir_path}")
